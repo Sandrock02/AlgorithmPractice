1718c5594b41f7bd601dc572514bfb8dd06eb7cf
Wenlei Huang <wenlei.huang@autodesk.com>
2018-03-19 12:28:41 +0800

    Add resume question Convert string to int

---
+++ AlgorithmPractice/ResumeQuestions/Q507ConvertStringToInt.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice.ResumeQuestions
+{
+    public static class Q507ConvertStringToInt
+    {
+        public static int ConvertString(string str, IFormatProvider formatProvider = null)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            var formatInfo = NumberFormatInfo.GetInstance(formatProvider ?? CultureInfo.CurrentCulture);
+
+            int cur = 0;
+            int sign = 1;
+            int num = 0;
+            int c = 0;
+            while (cur < str.Length)
+            {
+                char ch = str[cur];
+
+                if (IsMatching(str, ref cur, formatInfo.PositiveSign)) sign = 1;
+                else if (IsMatching(str, ref cur, formatInfo.NegativeSign)) sign = -1;
+                else if (IsMatching(str, ref cur, formatInfo.NumberDecimalSeparator)) break;
+                else if (IsDigital(ch))
+                {
+                    c = ch - '0';
+
+                    if (sign > 0 && num >= int.MaxValue / 10 && c > int.MaxValue % 10)
+                    {
+                        throw new Exception("The input number is greater than 2147483647.");
+                    }
+                    else if (sign < 0 && num >= (int.MaxValue / 10) && c > int.MaxValue % 10 + 1)
+                    {
+                        throw new Exception("The input number is smaller than -2147483649.");
+                    }
+
+                    num = num * 10 + c;
+                }
+                else
+                {
+                    throw new Exception($"There is unsupported character in {nameof(str)}.");
+                }
+
+                cur++;
+            }
+
+            return num * sign;
+        }
+
+        private static bool IsDigital(char c)
+        {
+            return c >= '0' && c <= '9';
+        }
+
+        private static bool IsMatching(string str, ref int startIndex, string pattern)
+        {
+            if (string.IsNullOrEmpty(str) && string.IsNullOrEmpty(pattern)) return true;
+            if (string.IsNullOrEmpty(str)) return false;
+            if (string.IsNullOrEmpty(pattern)) return true;
+
+            var result = str.Substring(startIndex).StartsWith(pattern);
+            if (result) startIndex += pattern.Length - 1;
+            return result;
+        }
+    }
+}
---
+++ AlgorithmPracticeTest/MicrosoftResumeQuestionsTests/Q507ConvertStringToIntTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using AlgorithmPractice.ResumeQuestions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmPracticeTest.MicrosoftResumeQuestionsTests
+{
+    [TestClass]
+    public class Q507ConvertStringToIntTests
+    {
+        [TestMethod]
+        public void TestConvertString_Normal()
+        {
+            var input = "123456";
+            var actual = Q507ConvertStringToInt.ConvertString(input);
+            Assert.AreEqual(123456, actual);
+        }
+
+        [TestMethod]
+        public void TestConvertString_PositiveValue()
+        {
+            var input = "+123456";
+            var actual = Q507ConvertStringToInt.ConvertString(input);
+            Assert.AreEqual(123456, actual);
+        }
+
+        [TestMethod]
+        public void TestConvertString_NagetiveValue()
+        {
+            var input = "-123456";
+            var actual = Q507ConvertStringToInt.ConvertString(input);
+            Assert.AreEqual(-123456, actual);
+        }
+
+        [TestMethod]
+        public void TestConvertString_MaxInt()
+        {
+            var input = "2147483647";
+            var actual = Q507ConvertStringToInt.ConvertString(input);
+            Assert.AreEqual(2147483647, actual);
+        }
+
+        [TestMethod]
+        public void TestConvertString_MinInt()
+        {
+            var input = "-2147483648";
+            var actual = Q507ConvertStringToInt.ConvertString(input);
+            Assert.AreEqual(-2147483648, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestConvertString_EmptyString()
+        {
+            var input = "";
+            Q507ConvertStringToInt.ConvertString(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "There is unsupported character in str.")]
+        public void TestConvertString_AllNonNumString()
+        {
+            var input = "aaaaaaa";
+            Q507ConvertStringToInt.ConvertString(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "The input number is greater than 2147483647.")]
+        public void TestConvertString_MaxOverflow()
+        {
+            var input = "2147483648";
+            Q507ConvertStringToInt.ConvertString(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "The input number is smaller than -2147483648.")]
+        public void TestConvertString_MinOverflow()
+        {
+            var input = "-2147483649";
+            Q507ConvertStringToInt.ConvertString(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "There is unsupported character in str.")]
+        public void TestConvertString_IncludeNonNumString()
+        {
+            var input = "12345a";
+            Q507ConvertStringToInt.ConvertString(input);
+        }
+
+        [TestMethod]
+        public void TestConvertString_IncludeDotString()
+        {
+            var input = "12345.6";
+            var actual = Q507ConvertStringToInt.ConvertString(input);
+            Assert.AreEqual(12345, actual);
+        }
+    }
+}
