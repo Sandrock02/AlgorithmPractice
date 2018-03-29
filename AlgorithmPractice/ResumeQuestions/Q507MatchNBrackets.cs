1d1b2900304a2a890e52f6f03529b5aec84fdf1d
Wenlei Huang <wenlei.huang@autodesk.com>
2018-03-29 16:27:59 +0800

    Add Q507 Final all available combinations of n pair brackets

---
+++ AlgorithmPractice/ResumeQuestions/Q507MatchNBrackets.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice.ResumeQuestions
+{
+    public class Q507MatchNBrackets
+    {
+        public static List<string> GenerateNBracketsCombination(uint bracketPair)
+        {
+            HashSet<string> result = new HashSet<string>();
+            if (bracketPair > 0)
+            {
+                Stack<char> pathStack = new Stack<char>();
+
+                TryMatchBrackets(pathStack, '(', 1, 0, result, bracketPair);
+            }
+
+            return result.ToList();
+        }
+
+        private static void TryMatchBrackets(Stack<char> path, char cur, uint left, uint right, HashSet<string> result, uint bracketPair)
+        {
+            path.Push(cur);
+            if (left == bracketPair && left == right)
+            {
+                // Finish construct the string, output the stack as result.
+                var str = new string(path.Reverse().ToArray());
+                if (!result.Contains(str))
+                {
+                    result.Add(str);
+                }
+
+                path.Pop();
+                return;
+            }
+
+            if (left >= right)
+            {
+                if (left < bracketPair)
+                {
+                    TryMatchBrackets(path, '(', left + 1, right, result, bracketPair);
+                }
+
+                if (right < bracketPair)
+                {
+                    TryMatchBrackets(path, ')', left, right + 1, result, bracketPair);
+                }
+            }
+
+            path.Pop();
+        }
+    }
+}
---
+++ AlgorithmPracticeTest/MicrosoftResumeQuestionsTests/Q507MatchNBracketsTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmPractice.ResumeQuestions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmPracticeTest.MicrosoftResumeQuestionsTests
+{
+    [TestClass]
+    public class Q507MatchNBracketsTests
+    {
+        [TestMethod]
+        public void TestGenerateNBracketsCombination()
+        {
+            var actual = Q507MatchNBrackets.GenerateNBracketsCombination(1);
+            var expect = new []{"()"};
+            CollectionAssert.AreEquivalent(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestGenerateNBracketsCombination2()
+        {
+            var actual = Q507MatchNBrackets.GenerateNBracketsCombination(2);
+            var expect = new[] { "()()", "(())" };
+            CollectionAssert.AreEquivalent(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestGenerateNBracketsCombination3()
+        {
+            var actual = Q507MatchNBrackets.GenerateNBracketsCombination(3);
+            var expect = new[] { "()()()", "()(())", "(())()", "(()())",  "((()))" };
+            CollectionAssert.AreEquivalent(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestGenerateNBracketsCombination4()
+        {
+            var actual = Q507MatchNBrackets.GenerateNBracketsCombination(4);
+            var expect = new[]
+            {
+                "()()()()", "()()(())", "()(())()", "()(()())", "()((()))",
+                            "(())()()",             "(()())()", "((()))()",
+                "(()()())", "(()(()))", "((())())", "((()()))", "(((())))",
+                "(())(())"
+            };
+            PrintActual(actual);
+            CollectionAssert.AreEquivalent(expect, actual);
+            
+        }
+
+        private void PrintActual(List<string> result)
+        {
+            Console.WriteLine("=============================");
+            foreach (var r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
